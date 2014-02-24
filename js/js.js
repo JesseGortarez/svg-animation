538274c57060c89192d4c6a441298cc24c9cbd92
jgortarez <jgortarez@gmail.com>
2014-02-23 18:22:02 -0800

    Adding support for multiple use per page with ID argument

--- js/js.js
+++ js/js.js
@@ -1,11 +1,12 @@
-$(function(){
+$(function () {
 
     'use strict';
 
-    var runAnimation = function(){
+    var runAnimation = function(id){
+        var id = '#'+id;
         var pathsArray = [];
 
-        $('path').each(function(){
+        $(id + ' path').each(function(){
             pathsArray.push(this);
         });
 
@@ -18,18 +19,23 @@ $(function(){
             pathsArray[i].style.strokeWidth = '1';
             pathsArray[i].getBoundingClientRect();
 
-            pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'fill 3s 2s ease forwards, fade 1s 6.2s ease';
+            pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'fill 3s 2s ease forwards, fade 1s 11.2s ease';
+            // This gives problems with Mozilla
+            // pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'stroke-dashoffset 2s 3s ease-in-out';
             pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'stroke-dashoffset ease-in-out';
 
 
-            pathsArray[i].style.transitionDuration = Math.floor(Math.random() * (3000 - 1000 + 1000)) + 1000;
-            pathsArray[i].style.transitionDelay = Math.floor(Math.random() * (1500 - 500 + 1000)) + 500;
+            pathsArray[i].style.transitionDuration = Math.floor(Math.random() * (4000 - 1000 + 1000)) + 1000;
+            pathsArray[i].style.transitionDelay = Math.floor(Math.random() * (2000 - 500 + 1000)) + 500;
             pathsArray[i].style.strokeDashoffset = '0';
 
         }
     };
-    runAnimation();
+
+    runAnimation('Layer_1');
     window.setInterval(function(){
-        runAnimation();
-    }, 7000);
+        runAnimation('Layer_1');
+        clearInterval();
+    }, 12000);
+
 });
