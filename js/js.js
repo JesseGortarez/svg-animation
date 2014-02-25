a59874b950459640766c43fe8fae1eac381c7174
jgortarez <jgortarez@gmail.com>
2014-02-24 20:37:41 -0800

    Updated code to allow for expandability - can now add different animations as arguments

--- js/js.js
+++ js/js.js
@@ -1,41 +1,54 @@
 $(function () {
-
     'use strict';
 
-    var runAnimation = function(id){
+    var runAnimation = function(id, animation){
         var id = '#'+id;
-        var pathsArray = [];
+        var animationName = animation;
 
-        $(id + ' path').each(function(){
-            pathsArray.push(this);
-        });
+        $(id).css('opacity', '0');
+        if ($(id).hasClass('present')){
+            $(id).css('opacity', '1');
+            var pathsArray = [];
 
-        for(var i = 0; i < pathsArray.length; i++) {
-            pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'none';
-            pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'none';
-            var length = pathsArray[i].getTotalLength();
-            pathsArray[i].style.strokeDasharray = length + ' ' + length;
-            pathsArray[i].style.strokeDashoffset = length;
-            pathsArray[i].style.strokeWidth = '1';
-            pathsArray[i].getBoundingClientRect();
+            $(id + ' path').each(function(){
+                pathsArray.push(this);
+            });
 
-            pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'fill 3s 2s ease forwards, fade 1s 11.2s ease';
-            // This gives problems with Mozilla
-            // pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'stroke-dashoffset 2s 3s ease-in-out';
-            pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'stroke-dashoffset ease-in-out';
+            for(var i = 0; i < pathsArray.length; i++) {
+                pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'none';
+                pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'none';
+                var length = pathsArray[i].getTotalLength();
+                pathsArray[i].style.strokeDasharray = length + ' ' + length;
+                pathsArray[i].style.strokeDashoffset = length;
+                pathsArray[i].style.strokeWidth = '1';
+                pathsArray[i].getBoundingClientRect();
+
+                if(animationName == "fill") {
+                    pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'fill 5s 3.5s ease forwards, fade 1s 11.2s ease';
+                    pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'stroke-dashoffset ease-in-out';
 
 
-            pathsArray[i].style.transitionDuration = Math.floor(Math.random() * (4000 - 1000 + 1000)) + 1000;
-            pathsArray[i].style.transitionDelay = Math.floor(Math.random() * (2000 - 500 + 1000)) + 500;
-            pathsArray[i].style.strokeDashoffset = '0';
+                    pathsArray[i].style.transitionDuration = Math.floor(Math.random() * (5000 - 2000 + 1000)) + 2000;
+                    pathsArray[i].style.transitionDelay = Math.floor(Math.random() * (2500 - 800 + 1000)) + 800;
+                    pathsArray[i].style.strokeDashoffset = '0';
+                }
+                else if (animationName == "fills") {
+                    pathsArray[i].style.animation = pathsArray[i].style.WebkitAnimation = 'fills 3s 2s ease forwards, fade 1s 11.2s ease';
+                    pathsArray[i].style.transition = pathsArray[i].style.WebkitTransition = 'stroke-dashoffset ease-in-out';
 
+
+                    pathsArray[i].style.transitionDuration = Math.floor(Math.random() * (4500 - 1000 + 1000)) + 1000;
+                    pathsArray[i].style.transitionDelay = Math.floor(Math.random() * (2000 - 500 + 1000)) + 500;
+                    pathsArray[i].style.strokeDashoffset = '0';
+                }
+            }
         }
     };
-
-    runAnimation('Layer_1');
-    window.setInterval(function(){
-        runAnimation('Layer_1');
-        clearInterval();
-    }, 12000);
+    $('#Layer_1').click(function(){
+        runAnimation('Layer_1', 'fills');
+        setInterval(function(){
+            runAnimation('Layer_1', 'fills');
+        }, 12000);
+    });
 
 });
